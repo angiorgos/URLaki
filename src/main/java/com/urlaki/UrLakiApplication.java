20700d918b6b32c24a9220d771c2484a55e1a9d8
angiorgos <angiorgos12@gmail.com>
2026-05-29 23:22:27 +0300

    initialize project with Maven setup and Spring Boot application

---
+++ src/main/java/com/urlaki/UrLakiApplication.java
@@ -0,0 +1,13 @@
+package com.urlaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrLakiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrLakiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/urlaki/UrLakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrLakiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
