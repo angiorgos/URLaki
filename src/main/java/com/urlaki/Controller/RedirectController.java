ff329c24e67708b9c143dc21dbba8fd2f06b5c2c
angiorgos <angiorgos12@gmail.com>
2026-06-03 16:07:44 +0300

    remove unnecessary annotations; eliminate RequestMapping and unused imports in RedirectController

--- src/main/java/com/urlaki/Controller/RedirectController.java
+++ src/main/java/com/urlaki/Controller/RedirectController.java
@@ -7,14 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
-
 import java.net.URI;
 
 @Controller
-@RequestMapping("/")
 @RequiredArgsConstructor
 public class RedirectController {
 
